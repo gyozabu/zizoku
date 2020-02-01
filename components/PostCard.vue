78a940314a4d3eaa86d3d1faebf0a8cc71bc7ae1
koharu goto <smspring0426@gmail.com>
2020-02-01 21:52:39 +0900

    カードのスタイル・内容など変更

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -1,43 +1,97 @@
 <template>
-  <v-card>
-    <v-card-title>{{ post.title }}</v-card-title>
-    <v-vard-subtitle>
+  <v-card raised class="post-card">
+    <v-card-title class="title">
+      <v-icon v-if="mode === 'edit' && edit.done" class="icon" color="success">
+        mdi-check-circle
+      </v-icon>
+      {{ post.title }}
+    </v-card-title>
+    <v-card-subtitle>
       毎日{{ post.scheduleTimeStamp }}, {{ post.limitTimeStamp }}まで
-    </v-vard-subtitle>
-    <v-card-text>
+    </v-card-subtitle>
+    <v-card-text class="post-result">
       <div class="count">
-        <p>success: {{ post.successNum }}</p>
-        <p>failure: {{ post.failureNum }}</p>
+        <p class="text">
+          <span class="subject">達成できた数</span>
+          <span class="result">{{ post.successNum }}</span>
+        </p>
+        <p class="text">
+          <span class="subject">達成できなかった数</span>
+          <span class="result">{{ post.failureNum }}</span>
+        </p>
       </div>
       <div class="graph"></div>
     </v-card-text>
-    <v-card-actions>
-      <v-btn outlined color="success">
-        <v-icon left>mdi-emoticon</v-icon>
-        DONE
-      </v-btn>
-      <v-spacer />
-      <v-btn @click="showEditor" icon>
-        <v-icon>{{ isExpand ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
-    <v-expand-transition>
-      <div v-show="isExpand">
-        <v-divider />
-        <div class="option">
-          <span class="text">成功時ツイート</span>
-          <v-switch v-model="post.successOption" />
-        </div>
-        <div class="option">
-          <span class="text">失敗時ツイート</span>
-          <v-switch v-model="post.failureOption" />
-        </div>
-        <v-btn outlined>
-          <v-icon left>mdi-delete</v-icon>
-          削除する
+    <template v-if="mode === 'edit'">
+      <v-card-actions class="actions">
+        <v-btn
+          :disabled="edit.done"
+          @click="changeStatusDone"
+          depressed
+          color="success"
+          width="280px"
+        >
+          <v-icon left>mdi-emoticon</v-icon>
+          達成できた！
         </v-btn>
-      </div>
-    </v-expand-transition>
+        <v-spacer />
+        <v-btn @click="toggleEditor" icon>
+          <v-icon>
+            {{ isOpenEditor ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
+          </v-icon>
+        </v-btn>
+      </v-card-actions>
+      <v-expand-transition>
+        <div v-show="isOpenEditor" class="post-option">
+          <div class="option -title">
+            <v-text-field v-model="edit.title" label="達成したいこと" />
+          </div>
+          <div class="option -schedule-time">
+            <v-text-field
+              v-model="edit.scheduleTimeStamp"
+              @focus="toggleTimePicker"
+              label="実行時間"
+              readonly
+            />
+            <v-dialog v-model="isOpenScheduleTimeDialog" width="290px">
+              <v-time-picker
+                @input="toggleTimePicker"
+                v-model="edit.scheduleTimeStamp"
+                scrollable
+                format="24hr"
+              />
+            </v-dialog>
+          </div>
+          <div class="option -limit-date">
+            <v-text-field
+              v-model="edit.limitTimeStamp"
+              @focus="toggleDatePicker"
+              label="終了日"
+              readonly
+            />
+            <v-dialog v-model="isOpenLimitDateDialog" width="290px">
+              <v-date-picker
+                @input="toggleDatePicker"
+                v-model="edit.limitTimeStamp"
+                scrollable
+              />
+            </v-dialog>
+          </div>
+          <div class="option -tweet">
+            <span class="text">達成できた時ツイートする</span>
+            <v-switch v-model="edit.successOption" class="switch" inset />
+          </div>
+          <div class="option -tweet">
+            <span class="text">達成できなかった時ツイートする</span>
+            <v-switch v-model="edit.failureOption" class="switch" inset />
+          </div>
+          <v-btn class="option -delete" outlined>
+            <v-icon left>mdi-delete</v-icon>
+            削除する
+          </v-btn>
+        </div>
+      </v-expand-transition>
+    </template>
   </v-card>
 </template>
 <script>
@@ -46,17 +100,97 @@ export default {
     post: {
       type: Object,
       default: () => {}
+    },
+    mode: {
+      type: String,
+      default: 'readonly'
     }
   },
   data() {
     return {
-      isExpand: false
+      isOpenEditor: false,
+      isOpenLimitDateDialog: false,
+      isOpenScheduleTimeDialog: false,
+      edit: {
+        title: null,
+        limitTimeStamp: null,
+        scheduleTimeStamp: null,
+        insertTimeStamp: null,
+        updateTimeStamp: null,
+        successOption: null,
+        failureOption: null,
+        done: null
+      }
     }
   },
+  computed: {},
+  created() {
+    if (this.mode === 'readonly') return
+    const keys = Object.keys(this.edit)
+    Object.entries(this.post).forEach(([key, value]) => {
+      if (keys.find((k) => k === key)) this.edit[key] = value
+    })
+  },
   methods: {
-    showEditor() {
-      this.isExpand = !this.isExpand
+    toggleEditor() {
+      this.isOpenEditor = !this.isOpenEditor
+    },
+    toggleDatePicker() {
+      this.isOpenLimitDateDialog = !this.isOpenLimitDateDialog
+    },
+    toggleTimePicker() {
+      this.isOpenScheduleTimeDialog = !this.isOpenScheduleTimeDialog
+    },
+    changeStatusDone() {
+      if (this.edit.done) return
+      this.edit.done = true
     }
   }
 }
 </script>
+<style lang="scss">
+.post-card {
+  > .title > .icon {
+    margin-right: 5px;
+  }
+  > .actions {
+    padding: 0 16px 16px;
+  }
+}
+
+.post-result {
+  display: flex;
+
+  > .count > .text {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 5px;
+  }
+  > .count > .text > .subject {
+    margin-right: 30px;
+  }
+}
+
+.post-option {
+  padding: 16px;
+  > .option {
+    &.-tweet {
+      display: flex;
+      align-items: top;
+      justify-content: space-between;
+    }
+    &.-delete {
+      display: block;
+      margin-right: 0;
+      margin-left: auto;
+    }
+  }
+  > .option > .text {
+    color: rgba(0, 0, 0, 0.6);
+  }
+  > .option > .switch {
+    margin: 0;
+    padding: 0;
+  }
+}
+</style>
---
+++ pages/users/_id.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="post-list-component">
+    <div class="form"></div>
+    <div class="list">
+      <div :key="index" v-for="(post, index) in posts" class="card">
+        <post-card :post="post" mode="edit" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import PostCard from '~/components/PostCard'
+
+export default {
+  components: {
+    PostCard
+  },
+  data() {
+    return {
+      posts: [
+        {
+          userId: 1,
+          title: '朝走る',
+          limitTimeStamp: '2020-12-08 00:00:00',
+          scheduleTimeStamp: '7:00',
+          insertTimeStamp: '2020-02-01 00:00:00',
+          updateTimeStamp: '2020-02-01 00:00:00',
+          successNum: 1000,
+          failureNum: 10000,
+          successOption: true,
+          failureOption: false,
+          done: true
+        },
+        {
+          userId: 1,
+          title: '夜ごはん食べる',
+          limitTimeStamp: '2020-12-08 00:00:00',
+          scheduleTimeStamp: '20:00',
+          insertTimeStamp: '2020-02-01 00:00:00',
+          updateTimeStamp: '2020-02-01 00:00:00',
+          successNum: 0,
+          failureNum: 0,
+          successOption: true,
+          failureOption: false,
+          done: false
+        },
+        {
+          userId: 1,
+          title: 'とてもとてもとてもとても長いなにか達成したいことが書いてある',
+          limitTimeStamp: '2020-12-08 00:00:00',
+          scheduleTimeStamp: '23:00',
+          insertTimeStamp: '2020-02-01 00:00:00',
+          updateTimeStamp: '2020-02-01 00:00:00',
+          successNum: 0,
+          failureNum: 0,
+          successOption: true,
+          failureOption: false,
+          done: true
+        },
+        {
+          userId: 1,
+          title: '腹筋する',
+          limitTimeStamp: '2020-12-08 00:00:00',
+          scheduleTimeStamp: '23:00',
+          insertTimeStamp: '2020-02-01 00:00:00',
+          updateTimeStamp: '2020-02-01 00:00:00',
+          successNum: 0,
+          failureNum: 0,
+          successOption: true,
+          failureOption: false,
+          done: true
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss">
+.post-list-component {
+  > .list {
+  }
+  > .list > .card {
+    margin: 5px;
+  }
+}
+</style>
