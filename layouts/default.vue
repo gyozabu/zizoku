aa2aff71a64fb876f76cdb96233b384f73cf7b41
minako-ph <chanmina143@gmail.com>
2020-02-02 15:48:25 +0900

    Delate header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,5 @@
 <template>
   <v-app>
-    <!-- ヘッダー内容 -->
-    <v-app-bar app>
-      <v-toolbar-title>
-        <nuxt-link to="/" class="logo">zizoku</nuxt-link>
-      </v-toolbar-title>
-    </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
