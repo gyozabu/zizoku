a7c85b08ba28bae2c96430eeec895a0ba225bee3
minako-ph <chanmina143@gmail.com>
2020-02-02 14:58:10 +0900

    Delate:cosole.log

--- pages/new.vue
+++ pages/new.vue
@@ -145,12 +145,6 @@ export default {
         done: false
       }
       db.add(taskData)
-        .then(function(docRef) {
-          console.log('Document written with ID: ', docRef.id)
-        })
-        .catch(function(error) {
-          console.error('Error adding document: ', error)
-        })
     }
   }
 }
